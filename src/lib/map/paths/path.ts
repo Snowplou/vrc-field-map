99d5fe822b2992ea0a4522738cad3765aa632d75
Snowplou <snowplou01@gmail.com>
2023-01-20 16:16:03 -0800

    code generation

--- src/lib/map/paths/path.ts
+++ src/lib/map/paths/path.ts
@@ -11,8 +11,10 @@ export class Path {
     constructor() {
         register_insert_option({
             name: "Point",
-            on_select: (x, y) =>
-                this.path.push(new Point(x/inch_pixel_ratio, y/inch_pixel_ratio))
+            on_select: (x, y) => {
+                this.path.push(new Point(x/inch_pixel_ratio, y/inch_pixel_ratio));
+                // this.testing();
+            }
         });
 
         register_insert_option({
@@ -22,13 +24,35 @@ export class Path {
                 y /= inch_pixel_ratio;
 
                 this.path.push(new BezierCurve(x, y, x, y-15, x+20, y-15, x+20, y));
+                // this.testing();
             }
         });
+    }
+
+    rnd(val){
+        return Math.round(val * 10) / 10
+    }
+
+    testing(){
+        let code = ""
+        for(let point of this.path){
+            if(point.points){
+                // Bezier
+                code += "robot.followCurve(pos(" + this.rnd(point.points[0].x / inch_pixel_ratio) + ", " + this.rnd(point.points[0].y / inch_pixel_ratio) + "), pos(" + this.rnd(point.points[1].x / inch_pixel_ratio) + ", " + this.rnd(point.points[1].y / inch_pixel_ratio) + "), pos(" + this.rnd(point.points[2].x / inch_pixel_ratio / inch_pixel_ratio) + ", " + this.rnd(point.points[2].y / inch_pixel_ratio) + "), pos(" + + this.rnd(point.points[3].x / inch_pixel_ratio) + ", " + this.rnd(point.points[3].y / inch_pixel_ratio) + "));\n"
+            }
+            else {
+                // Point
+                code += "robot.driveTo(" + this.rnd(point.x / inch_pixel_ratio) + ", " + this.rnd(point.y / inch_pixel_ratio) + ");\n"
+            }
+        }
+        console.log(code)
     }
 
     @on("postrender")
     render(ctx: CanvasRenderingContext2D) {
         
+        this.testing();
+
         ctx.strokeStyle = "#DD0000";
         ctx.lineWidth = .5 * inch_pixel_ratio;
         ctx.lineCap = "butt";
